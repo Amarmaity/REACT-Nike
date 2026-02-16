5d224de767028429ffdb8d5ef1765e712214e9f2
Amar <amar.maity@delostylestudio.com>
2026-02-16 18:24:19 +0530

    add data base like Master_Category, sub_boys_catgory, sub_girls_category,sub_kids_category, casual_shoes

--- back-end/api/admin.py
+++ back-end/api/admin.py
@@ -1,5 +1,11 @@
+from re import sub
 from django.contrib import admin
-from .models import User
+from .models import Casual_Shoes, Master_Category, User, sub_boys_category, sub_girls_category, sub_kids_category
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Master_Category)
+admin.site.register(sub_boys_category)
+admin.site.register(sub_girls_category)
+admin.site.register(sub_kids_category)
+admin.site.register(Casual_Shoes)
--- back-end/api/models.py
+++ back-end/api/models.py
@@ -1,3 +1,6 @@
+import re
+from venv import create
+from django import db
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
@@ -53,3 +56,99 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username} - {self.email}"
+
+
+# Product model
+class Master_Category(models.Model):
+    boys_id = models.CharField(max_length= 100, unique=True, null=False, blank=False)
+    girls_id = models.CharField(max_length= 100, unique=True, null=False, blank=False)
+    kides_id = models.CharField(max_length= 100, unique=True, null=False, blank=False)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "master_category"
+
+    def __str__(self):
+        return f"{self.boys_id} - {self.girls_id} - {self.kides_id}"
+    
+
+# Boyes Category model
+class sub_boys_category(models.Model):
+    master_category = models.ForeignKey(Master_Category, on_delete=models.CASCADE, related_name='sub_categories')
+    sports_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    casual_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    pary_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    runnig_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "sub_boys_category"
+    
+    def __str__(self):
+        return f"{self.sports_shoes_identify} - {self.casual_shoes_identify} - {self.pary_shoes_identify} - {self.runnig_shoes_identify}"
+
+
+# Girls Category model
+class sub_girls_category(models.Model):
+    master_category = models.ForeignKey(Master_Category, on_delete=models.CASCADE, related_name='sub_girls_categories')
+    sports_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    casual_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    pary_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    runnig_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "sub_girls_category"
+    
+    def __str__(self):
+        return f"{self.sports_shoes_identify} - {self.casual_shoes_identify} - {self.pary_shoes_identify} - {self.runnig_shoes_identify}"
+
+
+# Kids Category model
+class sub_kids_category(models.Model):
+    master_category = models.ForeignKey(Master_Category, on_delete=models.CASCADE, related_name='sub_kids_categories')
+    sports_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    casual_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    pary_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    runnig_shoes_identify = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "sub_kids_category"
+    
+    def __str__(self):
+        return f"{self.sports_shoes_identify} - {self.casual_shoes_identify} - {self.pary_shoes_identify} - {self.runnig_shoes_identify}"
+
+
+# Casual shoe Model
+class Casual_Shoes(models.Model):
+    sub_boyes_id = models.ForeignKey(sub_boys_category, on_delete=models.CASCADE, related_name='casual_shoes_boys')
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    b_c_s_image = models.URLField(max_length=500, blank=True, null=True)
+    b_c_s_description = models.TextField(blank=False, null=False)
+    b_c_s_size = models.CharField(max_length=50, null=False, blank=False)
+    b_c_s_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    b_c_s_slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "casual_shoes_boys"
+    
+    def __str__(self):
+        return f"{self.name} - {self.b_c_s_price}"
+    
