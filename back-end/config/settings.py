701e6801e0ee4b4bc53bc651a677a6770235f2ea
Amar <amarmaity243@gmail.com>
2026-07-13 17:34:11 +0530

    add database url

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -8,6 +8,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 
 # --------------------------------------------------
@@ -121,6 +122,18 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
 
 # --------------------------------------------------
 # PASSWORD VALIDATION
